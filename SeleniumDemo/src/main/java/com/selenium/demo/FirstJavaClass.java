ffd95565eeaf64196ed55d65c0c2355753156d12
priyavels <priyasri1999@gmail.com>
2021-08-05 22:16:47 -0500

    updating first java class

--- SeleniumDemo/src/main/java/com/selenium/demo/FirstJavaClass.java
+++ SeleniumDemo/src/main/java/com/selenium/demo/FirstJavaClass.java
@@ -2,38 +2,32 @@ package com.selenium.demo;
 
 public class FirstJavaClass {
 
-
 	private static String getMyName() {
 
 		return "Java";
 	}
 
 	private static int getSumOfTwoNumber(int i, int j) {
-		int k=i+j;
+		int k = i + j;
 		return k;
 	}
 
 	private static int getSumOfnNumber(int i) {
-		int sum = ((i)*(i+1))/2;
+		int sum = ((i) * (i + 1)) / 2;
 		return sum;
 	}
 
-
 	public static void main(String[] args) {
-
+		System.out.println("inside Main Method");
 		String myName = getMyName();
-		System.out.println("My Name is "+myName);
+		System.out.println("My Name is " + myName);
 
-		int sum = getSumOfTwoNumber(10,15);
-		System.out.println("Sum of 10 and 15 = "+sum);
+		int sum = getSumOfTwoNumber(10, 15);
+		System.out.println("Sum of 10 and 15 = " + sum);
 
 		int sumOfnNumber = getSumOfnNumber(15);
-		System.out.println("Sum of 0..15 ="+sumOfnNumber);
-
+		System.out.println("Sum of 0..15 =" + sumOfnNumber);
 
 	}
-
-
-
 
 }
